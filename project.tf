77989d1bcb6b04e696c1426e11ed456d1321ada4
Maxwell Morris <48025321+maxmorrisio@users.noreply.github.com>
2022-12-31 16:29:05 +0000

    Correcting resource names per TF conventions.

--- project.tf
+++ project.tf
@@ -1,8 +1,8 @@
-resource "digitalocean_project" "site-project" {
+resource "digitalocean_project" "site_project" {
   name        = "${var.site_name}-project"
   description = var.description
   purpose     = "Web Application"
   environment = var.environment
-  resources = [digitalocean_app.site-app.urn]
+  resources = [digitalocean_app.site_app.urn]
   is_default = false
 }
