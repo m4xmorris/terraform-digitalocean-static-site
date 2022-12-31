8178875770835c08f8a3ff5d70ce6391a7ffda71
Maxwell Morris <48025321+maxmorrisio@users.noreply.github.com>
2022-12-31 16:28:28 +0000

    Correcting resource names per TF conventions.

--- dns.tf
+++ dns.tf
@@ -1,17 +1,17 @@
 resource "cloudflare_record" "root_record" {
   zone_id = var.cloudflare_zone_id
   name    = var.domain
-  value   = replace(digitalocean_app.site-app.default_ingress,"/(https://)|(/)/","")
+  value   = replace(digitalocean_app.site_app.default_ingress,"/(https://)|(/)/","")
   type    = "CNAME"
   ttl     = 3600
-  depends_on = [digitalocean_app.site-app]
+  depends_on = [digitalocean_app.site_app]
 }
 
 resource "cloudflare_record" "www_record" {
   zone_id = var.cloudflare_zone_id
   name    = "www.${var.domain}"
-  value   = replace(digitalocean_app.site-app.default_ingress,"/(https://)|(/)/","")
+  value   = replace(digitalocean_app.site_app.default_ingress,"/(https://)|(/)/","")
   type    = "CNAME"
   ttl     = 3600
-  depends_on = [digitalocean_app.site-app]
+  depends_on = [digitalocean_app.site_app]
 }
