bc1502a1d5d7717b09f0ad002fe43f24bea826bc
Maxwell Morris <48025321+maxmorrisio@users.noreply.github.com>
2023-01-01 14:08:21 +0000

    Removed page_rule priority due to conflict.

--- page_rules.tf
+++ page_rules.tf
@@ -1,8 +1,6 @@
 resource "cloudflare_page_rule" "delete_www" {
   zone_id = var.cloudflare_zone_id
   target = "www.${var.domain}/*"
-  priority = 2
-
   actions {
     forwarding_url {
       url = "https://${var.domain}/$1"
