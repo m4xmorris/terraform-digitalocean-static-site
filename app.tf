05f171f7fe9608035be79af157d6ee76cd8ad333
Maxwell Morris <48025321+maxmorrisio@users.noreply.github.com>
2023-01-02 20:23:56 +0000

    Added development preview component.

--- app.tf
+++ app.tf
@@ -20,6 +20,24 @@ resource "digitalocean_app" "site_app" {
         branch = var.source_branch
         deploy_on_push = true
       }
+      routes {
+        path = "/"
+      }
+    }
+    static_site {
+      name          = "${var.site_name}-preview"
+      source_dir    = var.source_dir
+      output_dir = var.output_dir
+      build_command = var.build_command
+
+      github {
+        repo = var.source_repo
+        branch = var.preview_source_branch
+        deploy_on_push = true
+      }
+      routes {
+        path = "/preview"
+      }
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,20 @@
-variable "do_token" {}
-variable "cloudflare_token" {}
-variable "site_name" {}
-variable "region" {}
-variable "description" {}
-variable "environment" {}
-variable "domain" {}
-variable "cloudflare_zone_id" {}
-variable "source_repo" {}
-variable "source_branch" {}
-variable "source_dir" {}
-variable "output_dir" {}
-variable "build_command" {}
+variable "do_token" {
+    description = "DigitalOcean API Token"
+    sensitive = true
+}
+variable "cloudflare_token" {
+    description = "Cloudflare API Token"
+    sensitive = true
+}
+variable "site_name" {description = "Name of site"}
+variable "region" {description = "Region to deploy to - https://shorturl.at/htvyK"}
+variable "description" {description = "Description of site (Shown in DO portal)"}
+variable "environment" {description = " One of Production/Development/Staging (Shown in DO portal)"}
+variable "domain" {description = "FQDN to setup for the site"}
+variable "cloudflare_zone_id" {description = "Zone ID of the Cloudflare domain to setup"}
+variable "source_repo" {description = "GitHub repo containing site source (example-user/example-repo)"}
+variable "source_branch" {description = "Branch of repo to use"}
+variable "source_dir" {description = "Directory within repo containg site source"}
+variable "output_dir" {description = "Directory where site files can be found post-build"}
+variable "build_command" {description = "Command to build site from source"}
+variable "preview_source_branch" {description = "Branch of repo to use for development preview"}
