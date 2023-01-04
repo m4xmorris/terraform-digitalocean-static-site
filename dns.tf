77f841b78e10af10388809e498dc53a6bc1290d9
Maxwell Morris <48025321+maxmorrisio@users.noreply.github.com>
2023-01-04 19:23:31 +0000

    Testing

--- dns.tf
+++ dns.tf
@@ -1,10 +1,17 @@
+locals {
+    dns_records = {
+        "www" = "${var.domain}"
+        "root" = "www.${var.domain}"
+    }
+}
+
 module "site_dns" {
     source  = "m4xmorris/dns-record/cloudflare"
     version = "1.1.0"
-    for_each = [
-        var.domain,
-        "www.${var.domain}"
-    ]
+    for_each = {
+        for k, v in locals.dns_records : k => v
+        if var.manage_dns
+    }
     cloudflare_zone_id = var.cloudflare_zone_id
     name = each.value
     type = "CNAME"
