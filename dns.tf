451ed302806f41183d5d904f803b60a5ed5aba7f
Maxwell Morris <48025321+maxmorrisio@users.noreply.github.com>
2023-01-04 18:55:38 +0000

    Testing conditional DNS module with `for_each`.

--- dns.tf
+++ dns.tf
@@ -1,19 +1,13 @@
-module "root_dns" {
-    count = var.manage_dns ? 1 : 0
+module "site_dns" {
     source  = "m4xmorris/dns-record/cloudflare"
     version = "1.1.0"
-    cloudflare_zone_id = var.cloudflare_zone_id
-    name = var.domain
-    type = "CNAME"
-    value = replace(digitalocean_app.site_app.default_ingress,"/(https://)|(/)/","")
-}
-
-module "www_dns" {
     count = var.manage_dns ? 1 : 0
-    source  = "m4xmorris/dns-record/cloudflare"
-    version = "1.1.0"
+    for_each = [
+        var.domain,
+        "www.${var.domain}"
+    ]
     cloudflare_zone_id = var.cloudflare_zone_id
-    name = "www.${var.domain}"
+    name = each.value
     type = "CNAME"
     value = replace(digitalocean_app.site_app.default_ingress,"/(https://)|(/)/","")
 }
