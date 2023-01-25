8d427624bc7f767d888fa7991beef5c6d23db43b
Maxwell Morris <48025321+maxmorrisio@users.noreply.github.com>
2023-01-25 13:20:33 +0000

    `terraform fmt`

--- variables.tf
+++ variables.tf
@@ -10,19 +10,19 @@ variable "site_name" {
 variable "region" {
   type        = string
   description = "Region to deploy to - https://shorturl.at/htvyK"
-  default = "lon"
+  default     = "lon"
 }
 
 variable "description" {
   type        = string
   description = "Description of site (Shown in DO portal)"
-  default = "Managed by Terraform"
+  default     = "Managed by Terraform"
 }
 
 variable "environment" {
   type        = string
   description = " One of Production/Development/Staging (Shown in DO portal)"
-  default = "Development"
+  default     = "Development"
 }
 
 variable "domain" {
